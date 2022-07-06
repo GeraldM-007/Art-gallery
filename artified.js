51b8f8826118f320531456bdc03d29f781b13491
GeraldM-007 <geraldmuchuku237@gmail.com>
2022-07-06 14:41:29 +0300

    first commit

--- artified.js
+++ artified.js
@@ -2,3 +2,10 @@ $(document).ready(function(){
     $('#birth-date').mask('00/00/0000');
     $('#phone-number').mask('0000-0000');
    })
+   /* login button */
+    function signIn(){
+        window.location.href= "signin.html";
+    }
+    function seeArtits(){
+        window.location.href= "artits/Dainjarvis.html";
+    }
