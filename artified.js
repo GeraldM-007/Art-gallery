08c02a937f783376420c355372290e443dddc7d2
GeraldM-007 <geraldmuchuku237@gmail.com>
2022-07-11 01:09:57 +0300

    first commit

--- artified.js
+++ artified.js
@@ -6,6 +6,24 @@ $(document).ready(function(){
     function signIn(){
         window.location.href= "signin.html";
     }
-    function seeArtits(){
-        window.location.href= "artits/Dainjarvis.html";
+    /* Jarvis */
+    function jarvis(){
+        window.location.href= "artists/Jarvis T/Jarvis T.html";
+    }
+    function jarvisB(){
+        window.location.href= "Jarvis T/Jarvis T.html";
+    }
+    /* Alex */
+    function alex(){
+        window.location.href= "artists/Alex Kimani/Alex Kimani.html";
+    }
+    function alexB(){
+        window.location.href= "Alex Kimani/Alex Kimani.html";
+    }
+    /* Amwatah */
+    function amwatah(){
+        window.location.href= "artists/Amwatah Obuya/Amwatah Obuya.html";
+    }
+    function amwatahB(){
+        window.location.href= "Amwatah Obuya/Amwatah Obuya.html";
     }
